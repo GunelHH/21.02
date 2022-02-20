414bd941270e9b0268346f0027b9be7ef1a45b48
GunelHH <gunelfh@code.edu.az>
2022-02-20 21:45:36 +0400

    Commit added

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace _21._02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,56 @@
+﻿using System;
+namespace _21._02
+{
+    public class User:Admin
+    {
+        string username
+        {
+            get
+            {
+                return "Zehmet olmasa en az 7 simvoldan istifade edin";
+            }
+            set
+            {
+                if (value.Length>6)
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine("Zehmet olmasa yeniden cehd edin");
+                }
+
+            }
+        }
+         string password
+        {
+            get
+            {
+                return "Zehmet olmasa 1 eded ve 1 boyuk herfden istifade edin";
+            }
+            set
+            {
+                for (int i = 0; i < value.Length; i++)
+                {
+                    while (value = Int32. TryParse( , out int )
+                    {
+
+                    }
+                }
+
+            }
+         }
+        public User(string Username ,string Password,)
+        {
+            username = Username;
+            password = Password.ToString();
+        }
+        public string UserInfo()
+        {
+            return $"Username:{username}\nPassword:{password}";
+        }
+
+        
+
+    }
+}
